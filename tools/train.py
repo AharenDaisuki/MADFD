71f6c4740b5d10a5e5fbc65f9fd5763f3edeee1d
m1ntANDcardam0m <xyli45-c@my.cityu.edu.hk>
2023-10-26 13:28:11 +0800

    add: train.py

--- tools/train.py
+++ tools/train.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 import torch.multiprocessing as mp
 import torch.nn.functional as F
+from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from torch.utils.data import BatchSampler
 from models.MAT import MAT
@@ -18,12 +19,24 @@ import cv2
 from utils import dist_average,ACC
 assert torch.cuda.is_available()
 
+PERIOD = 100
+
 def train(args):
     config = args.config
     
     seed = config.seed
     root = config.root
     batch_size = config.batch_size
+    lr = config.learning_rate
+    betas = config.adam_betas
+    weight_decay = config.weight_decay
+    step_size = config.scheduler_step
+    gamma = config.scheduler_gamma
+    epoch_n = config.epochs
+    resume = config.ckpt
+    
+    start_epoch = 0
+    logs = {}
 
     numpy.random.seed(seed)
     torch.manual_seed(seed)
@@ -36,8 +49,40 @@ def train(args):
     # validate_sampler=torch.utils.data.distributed.DistributedSampler(validate_dataset)
     train_sampler = BatchSampler(train_dataset, batch_size=batch_size, drop_last=False)
     validate_sampler = BatchSampler(validate_dataset, batch_size=batch_size, drop_last=False) 
-    train_loader = DataLoader(train_dataset, batch_size=config.batch_size,sampler=train_sampler,pin_memory=True,num_workers=config.workers)
-    validate_loader = DataLoader(validate_dataset, batch_size=config.batch_size,sampler=validate_sampler,pin_memory=True,num_workers=config.workers)
+    # train_loader = DataLoader(train_dataset, batch_size=config.batch_size,sampler=train_sampler,pin_memory=True,num_workers=config.workers)
+    # validate_loader = DataLoader(validate_dataset, batch_size=config.batch_size,sampler=validate_sampler,pin_memory=True,num_workers=config.workers)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size,sampler=train_sampler)
+    validate_loader = DataLoader(validate_dataset, batch_size=batch_size,sampler=validate_sampler)
+
+    model = MAT(**config.net_config)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay)
+    scheduler=torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+
+    if resume:
+        pass
+
+    torch.cuda.empty_cache()
+    for epoch in range(start_epoch, epoch_n):
+        logs['epoch'] = epoch
+        # train_sampler.set_epoch(epoch)
+        # train_sampler.dataset.next_epoch()
+        # run(logs=logs,data_loader=train_loader,net=net,optimizer=optimizer,local_rank=local_rank,config=config,AG=AG,phase='train')
+        # run(logs=logs,data_loader=validate_loader,net=net,optimizer=optimizer,local_rank=local_rank,config=config,phase='valid')
+        # net.module.auxiliary_loss.alpha*=config.alpha_decay
+        scheduler.step()
+        if epoch % PERIOD == 0:
+            torch.save(
+                {
+                    'logs': logs,
+                    'state_dict': model.module.state_dict(),
+                    'optimzer_state': optimizer.state_dict(),
+                    'scheduler_state': scheduler.state_dict()
+                },
+                f'' # TODO: checkpoint file path
+            )
+
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
